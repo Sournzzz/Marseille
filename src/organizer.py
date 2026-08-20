31ca183094d92f8de1087d7a09658cfa8474bd24
Sournzzz <dvaccjor@gmail.com>
2026-08-19 19:44:05 -0600

    Third commit, I added some edge cases into the get_extensions method in the Organizer class, slow and steady (I guess)

--- src/organizer.py
+++ src/organizer.py
@@ -4,7 +4,18 @@ from pprint import pprint
 
 
 class Organize:
-    def __init__(self, directory: Path | None) -> None:
+    common_compound_suffixes = [
+        ".tar.gz",
+        ".tar.bz2",
+        ".tar.txz",
+        ".tar.zst",
+        ".tar.lz",
+        ".tar.Z",
+        ".mesh.xml",
+        ".skeleton.xml",
+    ]
+
+    def __init__(self, directory) -> None:
         self.directory = directory
 
     def organizer(self):
@@ -12,16 +23,31 @@ class Organize:
             return
 
     def get_extensions(self):
+
         test_dict = dict_testing(self.directory)
 
         # I'M GOING TO MANAGE SOME EDGE CASES AT THE MOMENT
-        test_dict["file_extensions"] = [
-            "".join(ext.suffixes) for ext in test_dict["files"]
-        ]
+
+        test_dict["extensions"] = set()
+        for file in test_dict["files"]:
+            suffix_tracker = file.suffixes
+
+            if (
+                len(suffix_tracker) > 2
+                and "".join(suffix_tracker) in self.common_compound_suffixes
+            ):
+                test_dict["extensions"].add("".join(suffix_tracker).lower())
+
+            elif file.suffix:
+                test_dict["extensions"].add(file.suffix.lower())
+
+            else:
+                continue  # Here goes a logging
 
         return test_dict
 
 
 if __name__ == "__main__":
-    organizer_test = Organize(find_dir("Downloads_test"))
+    dir_path_test = find_dir("Downloads_test")
+    organizer_test = Organize(dir_path_test)
     pprint(organizer_test.get_extensions())
--- src/utilities.py
+++ src/utilities.py
@@ -4,23 +4,18 @@ from pathlib import Path
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
-""" 
-THERE ARE SOME BROKEN AND UNDECIDED THINGS SUCH AS RETURN VALUES AND SOME ERROR 
-MANAGEMENTS, LACKS SOME LOGS AND TYPE HINTS AS WELL, NEED TO FIX SOME STINKY 
-STUFF, dict_testing IS A SHITTY NAME AND HAS NO PREVENTING ERRORS THINGY
-"""
-
 
 def temporary_test_copy(directory):
     if not directory:
         logger.error("Directory doesn't exist, couln't make a copy")
-
+        return
     directory_name = directory.name + "_test"
     destination_directory = directory.parent / directory_name
 
     try:
         logger.info("Starting copy of the [%s] directory", directory.name)
         shutil.copytree(directory, destination_directory)
+
     except FileExistsError:
         logger.error(
             "The [%s] directory already exists. Couldn't make copy.", directory_name
@@ -44,15 +39,12 @@ def find_dir(dir_name: str) -> Path | None:
     return None
 
 
-def dict_testing(directory: Path | None):
-
+def dict_testing(directory: Path):
+    if not directory:
+        return
     result = {}
-    result["files"] = [
-        Path(file.name) for file in directory.iterdir() if file.is_file()
-    ]
-    result["dirs"] = [
-        Path(folder.name) for folder in directory.iterdir() if folder.is_dir()
-    ]
+    result["files"] = [file for file in directory.iterdir() if file.is_file()]
+    result["dirs"] = [folder for folder in directory.iterdir() if folder.is_dir()]
 
     return result
 
